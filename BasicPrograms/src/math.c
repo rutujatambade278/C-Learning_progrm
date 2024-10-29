af682b46f421b831decb2182ce3f537f40f1712c
Rutuja <rutujatambade278@gmail.com>
2024-10-29 15:26:35 +0530

    c program

---
+++ BasicPrograms/src/math.c
@@ -0,0 +1,29 @@
+#include<math.h>
+int addtion(int num1, int num2){
+    int result=num1+num2;
+    return result;
+}
+
+int subtraction(int num1, int num2)
+{
+    int result = num1 - num2;
+    return result;
+}
+
+int multiplication(int num1, int num2)
+{
+    int result = num1 * num2;
+    return result;
+}
+
+int division(int num1, int num2)
+{
+    int result = num1 / num2;
+    return result;
+}
+
+int modulus(int num1, int num2)
+{
+    int result = num1 % num2;
+    return result;
+}
